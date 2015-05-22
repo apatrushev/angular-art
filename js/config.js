d7882db963aa9600a872ca4f31d331b735e65866
Anton Patrushev <apatrushev@gmail.com>
2015-05-22 12:03:04 +0200

    support for minified deps

--- js/config.js
+++ js/config.js
@@ -1,23 +1,29 @@
 ;(function (undefined) {
-    var local = false,
+    var local = true,
         fetch = [
             'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.17/require.js',
             'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.js',
             'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.js',
         ];
 
-    function fileName(path) {
-        return path.split('/').slice(-1).pop();
-    };
-
-    function removeExt(path) {
-        return path.split('.').slice(0, -1).join('.');
-    };
-
     function buildDeps(fetch, local) {
         return fetch.reduce(function(paths, path) {
-            var name = removeExt(fileName(path));
-            paths[name] = local?('deps/' + name):removeExt(path);
+            //remove .js extension from path and fileName
+            if (path.slice(-3) === '.js') {
+                path = path.slice(0, -3);
+            };
+
+            //get filename from path
+            var fileName = path.slice(path.lastIndexOf('/')+1);
+
+            //cut min from moduleName if present
+            var moduleName = fileName;
+            if (moduleName.slice(-4) === '.min') {
+                moduleName = moduleName.slice(0, -4);
+            };
+
+            paths[moduleName] = local?('deps/' + fileName):path;
+
             return paths;
         }, {});
     };
